ae8132c45c6e5c1add5e6cda0f9fd57915a1a45a
Eduin Frank Garcia Cordero <eduinlight@gmail.com>
2024-12-07 16:15:37 +0100

    save

--- wezterm.lua
+++ wezterm.lua
@@ -8,10 +8,13 @@ config.color_scheme = 'GruvboxDark'
 config.window_background_opacity = 0.95
 
 -- general
+config.window_close_confirmation = 'NeverPrompt'
 config.enable_tab_bar = false
 
 --font
 config.font = wezterm.font 'FiraCode Nerd Font'
 config.font_size = 11.0
+-- config.harfbuzz_features = { 'zero' }
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 return config
